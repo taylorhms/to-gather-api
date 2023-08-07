06cb49aa41bd07432aad1b96799c2b024d8223ba
taylorhms <taylorhenriquems@gmail.com>
2023-08-07 01:48:40 -0300

    envio de mensagens

--- src/main/java/com/togather/api/repository/MensagemRepository.java
+++ src/main/java/com/togather/api/repository/MensagemRepository.java
@@ -1,9 +1,12 @@
 package com.togather.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.togather.api.entity.Mensagem;
 
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
     
+    List<Mensagem> findByOrderByDataEnvioDesc();
 }
