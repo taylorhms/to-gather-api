dfe2828969de9cd2bde52645d4499ee24dd4b680
taylorhms <taylorhenriquems@gmail.com>
2023-08-12 17:16:24 -0300

    inclusão de dados do perfil do usuário

--- src/main/java/com/togather/api/entity/Usuario.java
+++ src/main/java/com/togather/api/entity/Usuario.java
@@ -7,11 +7,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,7 +29,7 @@ public class Usuario implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome")
+    @Column(name = "nome", nullable = false)
     private String nome;
 
     @Column(name = "login", unique = true, nullable = false)
@@ -34,6 +37,16 @@ public class Usuario implements UserDetails {
 
     @Column(name = "senha", nullable = false)
     private String senha;
+
+    @Column(name = "cor_primaria", nullable = false)
+    private String corPrimaria;
+
+    @Column(name = "cor_secundaria", nullable = false)
+    private String corSecundaria;
+
+    @Lob @Basic(fetch = FetchType.LAZY)
+    @Column(name = "foto")
+    private byte[] foto;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO usuarios (login, nome, senha) VALUES ('user', 'admin', '$2a$12$WF/axVQfbhfN8vaUSrHVTu1hJJYtPaAfnaFlQR2FkMccyDm1OBpC.');
+INSERT INTO usuarios (login, nome, senha, cor_primaria, cor_secundaria)
+    VALUES ('admin', 'Administrador', '$2a$12$WF/axVQfbhfN8vaUSrHVTu1hJJYtPaAfnaFlQR2FkMccyDm1OBpC.', 'violet', 'blue');
