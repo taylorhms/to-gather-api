e73de2e980dd790a5037234a3051f7ae4953457b
taylorhms <taylorhenriquems@gmail.com>
2023-08-13 12:07:18 -0300

    liberação de endpoints sem token

--- src/main/java/com/togather/api/security/SecurityConfig.java
+++ src/main/java/com/togather/api/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +44,13 @@ public class SecurityConfig {
             .httpBasic(Customizer.withDefaults())
             .addFilterBefore(filterToken, UsernamePasswordAuthenticationFilter.class)
             .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                new AntPathRequestMatcher("/login", "POST"),
+                new AntPathRequestMatcher("/usuario", "POST"));
     }
 
     @Bean
